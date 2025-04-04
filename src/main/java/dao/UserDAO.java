8175a3feba1d488ff10d4e1c97c67b91e7520603
Janeider Rojas <jrojas@cobiscorp.com>
2025-04-03 20:48:24 -0500

    JRT - Se quita print innecesario

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -95,7 +95,6 @@ public class UserDAO {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        System.out.println(userValidated);
         return userValidated;
     }
 }
